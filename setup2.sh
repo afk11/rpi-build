9830e57de920b843b4b4a1903a8434155b12d0ca
Thomas Kerin <thomas.kerin@bitmaintech.com>
2019-01-23 17:27:51 +0000

    update

--- setup2.sh
+++ setup2.sh
@@ -2,35 +2,25 @@
 
 ### You can write the raspbian image onto the sd card,
 # boot the pi so it expands the fs, then plug back to your laptop/desktop
-# and chroot to it with my script 
+# and chroot to it with my script
 # https://gist.github.com/htruong/7df502fb60268eeee5bca21ef3e436eb
 # sudo ./chroot-to-pi.sh /dev/sdb
 # I found it to be much less of a pain in the ass and more reliable
 # than doing the kpartx thing
-
-# install dependecies
-apt-get install qemu qemu-user-static binfmt-support
-
-# download raspbian image
-wget https://downloads.raspberrypi.org/raspbian_latest
-
-
-
-# extract raspbian image
-unzip raspbian_latest
+RASPBIAN=$(ls *.img)
 
 # extend raspbian image by 1gb
-dd if=/dev/zero bs=1M count=1024 >> 2017-11-29-raspbian-stretch-lite.img
+dd if=/dev/zero bs=1M count=1024 >> ${RASPBIAN}
 
 # set up image as loop device
-kpartx -v -a 2017-11-29-raspbian-stretch-lite.img
+kpartx -v -a ${RASPBIAN}
 
 #do the parted stuff, unmount kpartx, then mount again
 parted /dev/loop0
     resizepart 2 -1s
     quit
 kpartx -d /dev/loop0
-kpartx -v -a 2017-11-29-raspbian-stretch-lite.img
+kpartx -v -a ${RASPBIAN}
 
 # check file system
 e2fsck -f /dev/mapper/loop0p2
